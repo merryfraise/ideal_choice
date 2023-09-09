6261feeca6352d922015d1b01ce333160d6e39c2
Shoupeach <shoutrawberry@gmail.com>
2023-09-09 19:09:49 +0900

    🍒 feat: Headings component add h3 tag - atoms

--- src/components/atoms/Headings.tsx
+++ src/components/atoms/Headings.tsx
@@ -11,6 +11,8 @@ export default function Headings({ heading, children }: HeadingsProps) {
       return <h1>{children}</h1>;
     case 'h2':
       return <h2>{children}</h2>;
+    case 'h3':
+      return <h3>{children}</h3>;
     default:
       return null;
   }
