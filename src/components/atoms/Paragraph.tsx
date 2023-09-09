a76550c3bd5d60d344504da15a82bd942c42f2e3
Shoupeach <shoutrawberry@gmail.com>
2023-09-09 16:00:44 +0900

    🍒 feat: Feature of Paragraph component - atoms

--- src/components/atoms/Paragraph.tsx
+++ src/components/atoms/Paragraph.tsx
@@ -1,1 +1,9 @@
-export default function ParagraphComponent() {}
+import React from 'react';
+
+type ParagraphProps = {
+  children: React.ReactNode;
+};
+
+export default function ParagraphComponent({ children }: ParagraphProps) {
+  return <p>{children}</p>;
+}
