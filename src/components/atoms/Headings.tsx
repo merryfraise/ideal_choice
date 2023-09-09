86e1a6dbf4140a9e018a8d6e5d0fac045c5b4465
Shoupeach <shoutrawberry@gmail.com>
2023-09-09 15:20:50 +0900

    🍒 feat: Feature of Heading components - atoms

--- src/components/atoms/Headings.tsx
+++ src/components/atoms/Headings.tsx
@@ -1,1 +1,17 @@
-export default function HeadingsComponent() {}
+type HeadingsProps = {
+  heading: string;
+  text: string;
+};
+
+export default function HeadingsComponent(props: HeadingsProps) {
+  const heading = props.heading;
+
+  switch (heading) {
+    case 'h1':
+      return <h1>{props.text}</h1>;
+    case 'h2':
+      return <h2>{props.text}</h2>;
+    default:
+      return null;
+  }
+}
