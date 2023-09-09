a43275bef8036573163800ce3f46203cc75790bf
Shoupeach <shoutrawberry@gmail.com>
2023-09-09 16:53:33 +0900

    🍒 feat: Feature of Navigation component - molecules

--- src/components/molecules/Navigation.tsx
+++ src/components/molecules/Navigation.tsx
@@ -1,1 +1,12 @@
-export default function Navigation() {}
+import React from 'react';
+import Text from '../atoms/Text';
+
+export default function Navigation() {
+  return (
+    <nav>
+      <Text>이상적인 소개</Text>
+      <Text>이상적인 응답자</Text>
+      <Text>이상적인 질문</Text>
+    </nav>
+  );
+}
