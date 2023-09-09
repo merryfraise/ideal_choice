618ea0dbdae389fd5f69734c1d30d6527cfd617d
Shoupeach <shoutrawberry@gmail.com>
2023-09-09 16:54:18 +0900

    🍒 feat: Header component import Navigation component - molecules

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,9 +1,11 @@
 import Headings from '../atoms/Headings';
+import Navigation from '../molecules/Navigation';
 
 export default function Header() {
   return (
     <header>
       <Headings heading="h1">아마도 이상적인 선택들</Headings>
+      <Navigation />
     </header>
   );
 }
