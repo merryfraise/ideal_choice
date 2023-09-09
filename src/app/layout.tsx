661214c15e7f5fb229512866c6c0fe9518f31da3
Shoupeach <shoutrawberry@gmail.com>
2023-09-09 16:54:59 +0900

    🍏 chore: Basic Root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,24 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import React from 'react';
+import type { Metadata } from 'next';
+import Header from '@/components/organisms/Header';
 
-const inter = Inter({ subsets: ['latin'] })
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
 
 export const metadata: Metadata = {
   title: '아마도 이상적인 선택들',
-  description: '인간보다 이상적인 존재의 선택을 통해 우리는 더 멋진 세계를 만들어갈 수 있다.',
-}
+  description:
+    '인간보다 이상적인 존재의 선택을 통해 우리는 더 멋진 세계를 만들어갈 수 있다.',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
