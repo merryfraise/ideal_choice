4282057539ced093205eab5faf988f1f17dc57a7
Shoupeach <shoutrawberry@gmail.com>
2023-09-13 21:24:16 +0900

    🍓 style: Navigation component styling - molecules

--- src/components/molecules/Navigation.tsx
+++ src/components/molecules/Navigation.tsx
@@ -1,9 +1,23 @@
+/** @jsxImportSource @emotion/react */
+'use client';
+
 import React from 'react';
 import Text from '../atoms/Text';
+import { css } from '@emotion/react';
+
+const navStyle = css`
+  display: flex;
+  font-size: var(--medium);
+  color: var(--violet2);
+
+  > div {
+    margin-left: 40px;
+  }
+`;
 
 export default function Navigation() {
   return (
-    <nav>
+    <nav css={navStyle}>
       <Text>이상적인 소개</Text>
       <Text>이상적인 응답자</Text>
       <Text>이상적인 질문</Text>
