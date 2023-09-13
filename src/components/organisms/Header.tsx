52fa008f89ecfa8ffd544f511755cc77b8e1cbdd
Shoupeach <shoutrawberry@gmail.com>
2023-09-13 21:41:23 +0900

    🍓 style: Header component styling - organisms

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,9 +1,20 @@
+/** @jsxImportSource @emotion/react */
+'use client';
+
+import { css } from '@emotion/react';
 import Headings from '../atoms/Headings';
 import Navigation from '../molecules/Navigation';
 
+const headerStyle = css`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 34px;
+`;
+
 export default function Header() {
   return (
-    <header>
+    <header css={headerStyle}>
       <Headings heading="h1">아마도 이상적인 선택들</Headings>
       <Navigation />
     </header>
