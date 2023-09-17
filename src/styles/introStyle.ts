4cf94aa013050b66b6aac82628f881108dfdd1c7
Shoupeach <shoutrawberry@gmail.com>
2023-09-18 00:40:39 +0900

    🍒 feat: Modularize intro style

---
+++ src/styles/introStyle.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+const introStyle = css`
+  text-align: center;
+  font-size: 28px;
+  color: var(--violet3);
+  font-weight: var(--semibold-weight);
+  text-shadow: var(--violet-glow50);
+`;
+
+export default introStyle;
