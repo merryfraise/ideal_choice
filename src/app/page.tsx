412ba00162ceeaf24294cb4b48485939706d6899
Shoupeach <shoutrawberry@gmail.com>
2023-09-18 01:25:11 +0900

    🍓 style: Background image of main, intro pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import mainImg from '../../public/images/main_bg.png';
+
 export default function Home() {
-  return <main></main>;
+  return <Image src={mainImg} alt="background image" fill />;
 }
