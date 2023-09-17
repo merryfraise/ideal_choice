a1eb82251043678616952e8df757f1bd09ac0fd0
Shoupeach <shoutrawberry@gmail.com>
2023-09-18 00:42:56 +0900

    🍏 chore: Replace intro style with introStyle.ts

--- src/app/intro1/page.tsx
+++ src/app/intro1/page.tsx
@@ -2,19 +2,11 @@
 'use client';
 
 import Paragraph from '@/components/atoms/Paragraph';
-import { css } from '@emotion/react';
-
-const intro1Style = css`
-  text-align: center;
-  font-size: var(--large-font);
-  color: var(--violet3);
-  font-weight: var(--semibold-weight);
-  text-shadow: var(--violet-glow50);
-`;
+import introStyle from '@/styles/introStyle';
 
 export default function Page() {
   return (
-    <article css={intro1Style}>
+    <article css={introStyle}>
       <Paragraph>
         아마도 이상적인 선택들은 이상적인 응답자 바드의 탁월한 선택들을 기록하고
         제공합니다.
