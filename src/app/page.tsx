d2b7b7b0e1cc0e7e60301d735d86995b589c7f82
Shoupeach <shoutrawberry@gmail.com>
2023-09-21 01:22:39 +0900

    🍒 feat: Main page with logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import Text from '@/components/atoms/Text';
+import Logo from '@/components/organisms/Logo';
 import { css } from '@emotion/react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -14,22 +15,60 @@ const mainStyle = css`
   .logo {
     display: flex;
     justify-content: space-between;
+    align-items: center;
     font-size: var(--large-font);
     font-weight: var(--semibold-weight);
+  }
+
+  a {
+    .logo > div:nth-of-type(odd),
+    .start {
+      text-shadow: var(--violet-glow50);
+      transition: opacity ease-out 0.5s;
+    }
+
+    .straight_container,
+    .diagonal_container,
+    .center_container {
+      transition: transform ease-out 0.5s;
+    }
+  }
+
+  a:hover {
+    .logo > div:nth-of-type(odd),
+    .start {
+      opacity: 0;
+    }
+
+    .straight_container {
+      transform: rotate(-45deg);
+    }
+
+    .diagonal_container,
+    .center_container {
+      transform: rotate(45deg);
+    }
+  }
+
+  .start {
+    text-align: center;
   }
 `;
 
 export default function Home() {
   return (
-    <>
+    <section css={mainStyle}>
       <Image src="/images/main_bg.png" alt="background image" fill />
-      <Link href="/random" css={mainStyle}>
+      <Link href="/random">
         <div className="logo">
           <Text>A</Text>
+          <Logo />
           <Text>B</Text>
         </div>
-        <Text>눌러서 시작하기</Text>
+        <div className="start">
+          <Text>눌러서 시작하기</Text>
+        </div>
       </Link>
-    </>
+    </section>
   );
 }
