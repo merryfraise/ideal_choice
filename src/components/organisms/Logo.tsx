a5bb4306b346f202080e6a2b9380afc5c2c0188d
Shoupeach <shoutrawberry@gmail.com>
2023-09-21 00:13:48 +0900

    🍒 feat: Logo component - organisms

--- src/components/organisms/Logo.tsx
+++ src/components/organisms/Logo.tsx
@@ -2,12 +2,59 @@
 'use client';
 
 import { css } from '@emotion/react';
+import Image from 'next/image';
 
 const logoStyle = css`
-  width: 600px;
-  height: 600px;
+  position: relative;
+  width: 565px;
+  height: 565px;
+
+  img {
+    filter: drop-shadow(var(--violet-glow50));
+  }
+
+  .straight_container,
+  .diagonal_container,
+  .center_container {
+    position: absolute;
+  }
+
+  .diagonal_container,
+  .center_container {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 `;
 
 export default function Logo() {
-  return <div css={logoStyle}>로고</div>;
+  return (
+    <div className="img_logo" css={logoStyle}>
+      <div className="straight_container">
+        <Image
+          src="/images/logo_straight.png"
+          alt="logo"
+          width={565}
+          height={565}
+          priority={true}
+        />
+      </div>
+      <div className="diagonal_container">
+        <Image
+          src="/images/logo_diagonal.png"
+          alt="logo"
+          width={229}
+          height={229}
+        />
+      </div>
+      <div className="center_container">
+        <Image
+          src="/images/logo_center.png"
+          alt="logo"
+          width={42}
+          height={42}
+        />
+      </div>
+    </div>
+  );
 }
