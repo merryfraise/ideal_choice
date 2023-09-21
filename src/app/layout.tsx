0555992bcf6578b691722f60a89355778af48176
Shoupeach <shoutrawberry@gmail.com>
2023-09-21 12:44:31 +0900

    🍏 chore: Set favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
         <Header />
         <main>{children}</main>
