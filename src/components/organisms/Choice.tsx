117222645bfd7b31236f9e1b1d2b0c8c04d2a877
Shoupeach <shoutrawberry@gmail.com>
2023-09-25 00:45:24 +0900

    🍓 style: Choice component basic style

--- src/components/organisms/Choice.tsx
+++ src/components/organisms/Choice.tsx
@@ -1,6 +1,17 @@
+/** @jsxImportSource @emotion/react */
+'use client';
+
+import { css } from '@emotion/react';
+
+const choiceStyle = css`
+  width: 682px;
+  height: 100%;
+  border-right: solid 2px var(--violet1);
+`;
+
 export default function Choice() {
   return (
-    <aside>
+    <aside css={choiceStyle}>
       <div style={{ color: 'white' }}>테스트</div>
     </aside>
   );
