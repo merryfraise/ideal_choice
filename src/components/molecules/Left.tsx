c091c4ea0cb2c71dd98c470a71b2b5ade5f3e824
Shoupeach <shoutrawberry@gmail.com>
2023-09-29 21:45:57 +0900

    🍓 style: Left component style

--- src/components/molecules/Left.tsx
+++ src/components/molecules/Left.tsx
@@ -1,3 +1,69 @@
+/** @jsxImportSource @emotion/react */
+'use client';
+
+import Route from '@/utils/Route';
+import { css } from '@emotion/react';
+import Image from 'next/image';
+
+const leftStyle = css`
+  position: relative;
+  width: 360px;
+  height: 360px;
+
+  .direction_container,
+  .center_container {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+  }
+
+  .direction_container {
+    width: 360px;
+    height: 360px;
+    margin-left: -180px;
+    margin-top: -180px;
+  }
+
+  .center_container {
+    width: 28px;
+    height: 28px;
+    margin-left: -14px;
+    margin-top: -14px;
+  }
+
+  .logo_container:hover {
+    img {
+      filter: drop-shadow(var(--violet-glow50));
+    }
+  }
+`;
+
 export default function Left() {
-  return <></>;
+  const route = Route();
+  return (
+    <div css={leftStyle}>
+      {route.includes('/choice') ? null : (
+        <div className="logo_container">
+          <div className="direction_container">
+            <Image
+              src="/images/left_direction.png"
+              alt="logo"
+              width={360}
+              height={360}
+              priority={true}
+            />
+          </div>
+          <div className="center_container">
+            <Image
+              src="/images/left_center.png"
+              alt="logo"
+              width={28}
+              height={28}
+              priority={true}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
