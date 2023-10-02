ca5c37d071c290ecc88310f1a49eb40838d54acc
Shoupeach <shoutrawberry@gmail.com>
2023-10-02 21:43:33 +0900

    🍒 feat: Random page data fetch test

--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -1,7 +1,39 @@
+/** @jsxImportSource @emotion/react */
 'use client';
 
-import Choice from '@/components/organisms/Choice';
+import data from '@/database/data.json';
+import { css } from '@emotion/react';
+import Link from 'next/link';
+
+const RandomStyle = css`
+  ul {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  a {
+    color: var(--white);
+    font-size: 46px;
+    font-weight: var(--semibold-weight);
+  }
+
+  a:hover {
+    font-size: 48px;
+    text-shadow: var(--white-glow);
+  }
+`;
 
 export default function Random() {
-  return <></>;
+  return (
+    <div css={RandomStyle}>
+      <ul>
+        {data.choice.map((choice) => (
+          <li key={choice.id}>
+            <Link href={`/random/choice/${choice.id}`}>{choice.question}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
