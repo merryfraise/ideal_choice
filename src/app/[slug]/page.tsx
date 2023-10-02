4215cd1b37afda41a286c43ec4a4d2a6b3e7adf3
Shoupeach <shoutrawberry@gmail.com>
2023-10-03 00:36:51 +0900

    🍒 feat: Choiced page basic setting

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,44 @@
+/** @jsxImportSource @emotion/react */
+'use client';
+
 import data from '@/database/data.json';
+import { css } from '@emotion/react';
 import Link from 'next/link';
+import Headings from '@/components/atoms/Headings';
+import Paragraph from '@/components/atoms/Paragraph';
+
+const choicedStyle = css`
+  height: calc(100% - 116px);
+  padding: 34px 164px 116px 164px;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  a {
+    font-size: var(--small-font);
+  }
+
+  h1,
+  p {
+    color: var(--white);
+  }
+
+  h1 {
+    padding-top: 16px;
+    text-shadow: var(--white-glow);
+  }
+
+  p:not(:nth-of-type(2)) {
+    padding-top: 16px;
+  }
+
+  p:first-of-type,
+  p:nth-of-type(2) {
+    font-weight: var(--semibold-weight);
+  }
+`;
 
 type ChoicedPageProps = {
   params: {
@@ -13,10 +52,25 @@ export default function Choiced({ params }: ChoicedPageProps) {
   );
 
   return (
-    <>
-      {choicedData.map((data) => {
-        <Link href={`/random/choice/${params.slug}/hidden`}>{data.bard}</Link>;
-      })}
-    </>
+    <div css={choicedStyle}>
+      {choicedData.map((data) => (
+        <>
+          <Link href={`/${data.id}/hidden`} key={1}>
+            {data.chapter}
+          </Link>
+          <Headings heading="h1" key={2}>
+            {data.title}
+          </Headings>
+          <div className="text_conatiner">
+            <Paragraph key={3}>{data.first1}</Paragraph>
+            <Paragraph key={4}>{data.first2}</Paragraph>
+            <Paragraph key={5}>{data.p1}</Paragraph>
+            <Paragraph key={6}>{data.p2}</Paragraph>
+            <Paragraph key={7}>{data.p3}</Paragraph>
+            <Paragraph key={8}>{data.p4}</Paragraph>
+          </div>
+        </>
+      ))}
+    </div>
   );
 }
