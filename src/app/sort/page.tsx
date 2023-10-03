1c21875871e419a9570cc2b7128b52507e04b6e7
Shoupeach <shoutrawberry@gmail.com>
2023-10-03 14:46:10 +0900

    🍒 feat: Sort page data fetching

--- src/app/sort/page.tsx
+++ src/app/sort/page.tsx
@@ -1,3 +1,68 @@
+/** @jsxImportSource @emotion/react */
+'use client';
+
+import Text from '@/components/atoms/Text';
+import data from '@/database/data.json';
+import { css } from '@emotion/react';
+import Link from 'next/link';
+
+const sortStyle = css`
+  height: 100%;
+  padding-top: 116px;
+  overflow-y: scroll;
+
+  .sort_container {
+    border-top: solid 1px var(--violet1);
+  }
+
+  .subject_container {
+    margin: 10px;
+    color: var(--violet3);
+    font-size: var(--small-font);
+  }
+
+  .choice_container {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    padding: 0 0 34px 16px;
+  }
+
+  .choice_box {
+    margin-top: 34px;
+    margin-right: 16px;
+  }
+
+  a {
+    color: var(--white);
+    font-size: var(--small-font);
+  }
+
+  a:hover {
+    font-size: 26px;
+    text-shadow: var(--white-glow);
+  }
+`;
+
 export default function Sort() {
-  return <></>;
+  return (
+    <div css={sortStyle}>
+      {data.sort.map((sort) => (
+        <div className="sort_container" key={sort.id}>
+          <div className="subject_container">
+            <Text>{sort.subject}</Text>
+          </div>
+          <div className="choice_container">
+            {data.choice.map((choice) =>
+              sort.id === choice.sortId ? (
+                <div className="choice_box" key={`choice ${choice.id}`}>
+                  <Link href={`/${choice.id}`}>{choice.question}</Link>
+                </div>
+              ) : null
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
